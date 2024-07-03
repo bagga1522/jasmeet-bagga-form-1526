a3ed5f4b078d77ef8489c82a1619f3ef1d16e976
bagga1522 <bagg0058@algonquinlive.com>
2024-07-02 22:17:02 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,63 @@
+const button = document.getElementById('submit-button');
+
+const fullNameInput = document.getElementById('fullname');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+
+const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+
+
+function validateForm(event) {
+
+  
+  
+  event.preventDefault(); 
+  
+
+  let data = {};
+  let errors = [];
+
+  // Validating Full Name
+  if (fullNameInput.value.trim() === '') {
+    errors.push("Full Name is required.");
+  } else {
+    data.fullname = fullNameInput.value.trim();
+  }
+
+  // Validating Email
+  if (emailInput.value.trim() === '') {
+    errors.push("Email is required.");
+  } else if (!emailRegex.test(emailInput.value.trim())) {
+    errors.push("Invalid Email Address.");
+  } else {
+    data.email = emailInput.value.trim();
+  }
+
+  // Validating Message
+  if (messageInput.value.trim() === '') {
+    errors.push("Message is required.");
+  } else {
+    data.message = messageInput.value.trim();
+  }
+
+  // Displaying errors 
+  if (errors.length > 0) {
+    console.log(errors);
+
+  } else {
+    console.log("Form data:", data);
+    clearForm();
+  }
+}
+
+
+
+function clearForm() {
+  fullNameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+button.addEventListener('click', validateForm);
